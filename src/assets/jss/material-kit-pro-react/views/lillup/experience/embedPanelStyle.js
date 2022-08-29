489bc42beb21853a322c7f4c73a1b96404543582
scrubbybetter <ykovarov@gmail.com>
2022-08-29 09:57:36 +0900

    complete embedPanel by ko

--- src/assets/jss/material-kit-pro-react/views/lillup/experience/embedPanelStyle.js
+++ src/assets/jss/material-kit-pro-react/views/lillup/experience/embedPanelStyle.js
@@ -1,27 +1,129 @@
 import {
-  whiteColor,
-  blackColor,
-  grayColor,
-  hexToRgb,
+    primaryColor,
+    dangerColor,
+    roseColor,
+    grayColor,
+    blackColor,
+    whiteColor,
+    hexToRgb,
 } from "assets/jss/material-kit-pro-react.js";
 
 const embedPanelStyle = (theme) => ({
-	panelBgColor: {
-		backgroundColor: "yellow",
-	},
+    title: {
+        marginLeft: "25px",
+        color: whiteColor,
+        fontFamily: "Roboto",
+        fontSize: "20px",
+    },
 
-	panelSize: {
-		width: "auto",
-		height: "100%",
-	},
+    fontStyle: {
+        color: "#255FD0",
+        fontSize: "14px",
+        fontWeight: "bold",
+    },
 
-	dialog: {
+    dialog: {
 		backgroundColor: "#212937",
 		color: whiteColor,
 		maxWidth: "800px",
 		width: "800px",
 		height: "500px",
 	},
+
+    modalBody: {
+        paddingTop: "24px",
+        paddingRight: "24px",
+        paddingBottom: "16px",
+        paddingLeft: "24px",
+        position: "relative",
+        overflow: "visible",        
+    },
+    modalFooter: {
+        padding: "15px",
+        textAlign: "center",
+        paddingTop: "0",
+        margin: "0",
+    },
+
+    modalClose: {
+        width: "24px",
+        height: "24px",        
+    },
+
+    howToTitle: {
+        marginLeft: "25px",
+        fontFamily: "Roboto",
+        fontSize: "12px",    
+    },
+
+    howToInput: {
+        marginLeft: "25px",
+        marginTop: "10px",
+        width: "400px",
+        height: "30px",
+        backgroundColor: "#333C41",
+        color: whiteColor,
+    },
+
+    switchBase: {
+        color: primaryColor[0] + "!important",
+        "&:hover": {
+          backgroundColor:
+            "rgba(" + hexToRgb(primaryColor[0]) + ", 0.14) !important",
+        },
+      },
+      
+      switchIcon: {
+        boxShadow: "0 1px 3px 1px rgba(" + hexToRgb(blackColor) + ", 0.4)",
+        color: whiteColor + "  !important",
+        border: "1px solid rgba(" + hexToRgb(blackColor) + ", .54)",
+      },
+      
+      switchIconChecked: {
+        borderColor: primaryColor[0],
+        transform: "translateX(0px)!important",
+      },
+
+      switchBar: {
+        width: "30px",
+        height: "15px",
+        backgroundColor: "rgb(" + hexToRgb(grayColor[24]) + ")",
+        borderRadius: "15px",
+        opacity: "0.7!important",
+      },
+      
+      switchChecked: {
+        "& + $switchBar": {
+          backgroundColor: "rgba(" + hexToRgb(primaryColor[0]) + ", 1) !important",
+        },
+        "& $switchIcon": {
+          borderColor: primaryColor[0],
+        },
+      },
+
+      contentPanel: {
+        marginLeft: "25px",
+        marginTop: "20px",
+        width: "400px",
+        height: "150px",
+        backgroundColor: "#333C41",
+      },
+
+      copyBtn: {
+        backgroundColor: "#255FD0",
+        marginTop: "10px",
+        width: "400px",
+        height: "50px",
+        fontSize: "20px",
+      },
+
+      dismissBtn:{
+        backgroundColor: "#255FD0",
+        marginTop: "10px",
+        width: "400px",
+        height: "50px",
+        fontSize: "20px",
+      },
 
 });
 
--- src/views/LillupPage/LearnerExperiencePage/Desktop14Page.js
+++ src/views/LillupPage/LearnerExperiencePage/Desktop14Page.js
@@ -5,6 +5,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Remove from "@material-ui/icons/Remove";
 import Add from "@material-ui/icons/Add";
 import Close from "@material-ui/icons/Close";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 
 import ImageList from '@mui/material/ImageList';
 
@@ -25,6 +27,7 @@ import commonStyle from "assets/jss/material-kit-pro-react/views/lillup/experien
 import buttonGroupStyle from "assets/jss/material-kit-pro-react/buttonGroupStyle.js";
 import embedPanelStyle from "assets/jss/material-kit-pro-react/views/lillup/experience/embedPanelStyle.js";
 import javascriptStyles from "assets/jss/material-kit-pro-react/views/componentsSections/javascriptStyles.js";
+import basicsStyle from "assets/jss/material-kit-pro-react/views/componentsSections/basicsStyle.js";
 
 import imageBack from "assets/img/lillup-learner-experience-desktop-13.png";
 import imageMarkCopy from "assets/img/lillup/experience/Mark_Copy.png";
@@ -46,6 +49,7 @@ const useCommonStyles = makeStyles(commonStyle);
 const useButtonGroupStyle = makeStyles(buttonGroupStyle);
 const useEmbedPanelStyles = makeStyles(embedPanelStyle);
 const useJavascriptStyles = makeStyles(javascriptStyles);
+const useBasicStyles = makeStyles(basicsStyle);
 
 import NFTTokenPanel from "views/LillupPage/LearnerExperiencePage/NFTTokenPanel.js";
 
@@ -73,7 +77,7 @@ const useFormStyles = makeStyles((theme) => ({
 }));
 
 import { useMoralis, useNFTBalances } from "react-moralis";
-import { Dialog, DialogContent, DialogTitle, DialogActions } from "@material-ui/core";
+import { Dialog, DialogContent, DialogTitle, DialogActions, Input } from "@material-ui/core";
 import Slide from "@material-ui/core/Slide";
 import Instruction from "components/Instruction/Instruction.js";
 
@@ -98,6 +102,7 @@ export default function Desktop14Page({ ...rest }) {
   const [selectedWalletAddress, setSelectedWalletAddress] = useState("");
   const [selectedTokenID, setSelectedTokenID] = useState("");
   const [embeddedPanel, setEmbeddedPanel] = React.useState(false);
+  const [checkedA, setCheckedA] = React.useState(true);
 
   const [values, setValues] = React.useState({
     amount: '',
@@ -119,6 +124,7 @@ export default function Desktop14Page({ ...rest }) {
   const buttonGroupClasses = useButtonGroupStyle();
   const embedClasses = useEmbedPanelStyles();
   const javascriptStylesClasses = useJavascriptStyles();
+  const basicStyleClasses = useBasicStyles();
 
   const { authenticate, isAuthenticated, logout, user, auth } = useMoralis();
   const { getNFTBalances, data, error, isLoading, isFetching } = useNFTBalances();
@@ -678,49 +684,65 @@ export default function Desktop14Page({ ...rest }) {
                       onClick={() => setEmbeddedPanel(false)}
                     >
                       {" "}
-                      <Close className={javascriptStylesClasses.modalClose} />
+                      <Close className={embedClasses.modalClose} />
                     </Button>
-                    <h4 className={javascriptStylesClasses.modalTitle}>Embed maps in your site</h4>
+                    <span className={embedClasses.title}>Embed maps in your site</span>
                   </DialogTitle>
                   <DialogContent
-                    id="classic-modal-slide-description"
-                    className={javascriptStylesClasses.modalBody}
-                  >
-                    <p>
-                      Far far away, behind the word mountains, far from the
-                      countries Vokalia and Consonantia, there live the blind
-                      texts. Separated they live in Bookmarksgrove right at the
-                      coast of the Semantics, a large language ocean. A small
-                      river named Duden flows by their place and supplies it
-                      with the necessary regelialia. It is a paradisematic
-                      country, in which roasted parts of sentences fly into your
-                      mouth. Even the all-powerful Pointing has no control about
-                      the blind texts it is an almost unorthographic life One
-                      day however a small line of blind text by the name of
-                      Lorem Ipsum decided to leave for the far World of Grammar.
-                    </p>
+                    className={embedClasses.modalBody}
+                  >  
+                    <span className={embedClasses.howToTitle}>Learn more about how to embed the logic{" "}
+                      <a className={embedClasses.fontStyle}
+                        href="https://www.creative-tim.com/?ref=mkpr-javascript-components"
+                      >
+                        here
+                      </a>
+                    </span>
+                    <div>
+                      <OutlinedInput
+                        className={embedClasses.howToInput}
+                        type={'text'}
+                        endAdornment={<InputAdornment position="end">
+                          <FormControlLabel
+                            control={
+                              <Switch
+                                checked={checkedA}
+                                onChange={(event) => setCheckedA(event.target.checked)}
+                                value="checkedA"
+                                classes={{
+                                  switchBase: embedClasses.switchBase,
+                                  checked: embedClasses.switchChecked,
+                                  thumb: embedClasses.switchIcon,
+                                  track: embedClasses.switchBar,
+                                }}
+                              />
+                            }
+                            classes={{
+                              label: basicStyleClasses.label,
+                              root: basicStyleClasses.labelRoot,
+                            }}
+                          />
+                        </InputAdornment>}
+                        value={checkedA ? "iFrame" : "Web component"}
+                      />
+                    </div>
+                    <div>
+                      <Input className={embedClasses.contentPanel}>
 
-                    {/* <Instruction
-                      text={
-                        <span>Learn more about how to embed the logic{" "}
-                          <a
-                            href="https://www.creative-tim.com/?ref=mkpr-javascript-components"
-                          >
-                            here                            
-                          </a>
-                        </span>
-                      }
-                    /> */}
+                      </Input>
+                    </div>
                   </DialogContent>
-                  <DialogActions className={javascriptStylesClasses.modalFooter}>
-                    <Button link>Nice Button</Button>
-                    <Button
-                      onClick={() => setEmbeddedPanel(false)}
-                      color="danger"
-                      simple
-                    >
-                      Close
-                    </Button>
+                  <DialogActions className={embedClasses.modalFooter}>
+                    <div className={embedClasses.space}>
+                      <Button link className={embedClasses.copyBtn}>Copy</Button>
+                      <Button
+                        onClick={() => setEmbeddedPanel(false)}
+                        simple
+                        className={embedClasses.dismissBtn}
+                      >
+                        Dismiss
+                      </Button>
+                    </div>
                   </DialogActions>
                 </Dialog>
               </div>
